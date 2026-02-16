5161f8909640ed2fb14ca077059dee1ddc929e63
gamcewen <gavin.mcewen@colostate.edu>
2026-02-16 10:19:05 -0700

    Complete assignments for week 4

--- modules/wk_04_programming_basics/python_part_2_independent/assignment2.py
+++ modules/wk_04_programming_basics/python_part_2_independent/assignment2.py
@@ -6,7 +6,7 @@ def add_to_list(input_list, item):
 
     ### YOUR CODE BELOW HERE ###
 
-    print("\nReplace this with your code!\n")
+    input_list.insert(0, item)
 
     ### YOUR CODE ABOVE HERE ###
     
@@ -18,7 +18,7 @@ def merge_lists(list_1, list_2):
 
     ### YOUR CODE BELOW HERE ###
 
-    print("\nReplace this with your code!\n")
+    merged_list = list_1 + list_2
 
     ### YOUR CODE ABOVE HERE ###
 
@@ -30,7 +30,8 @@ def remove_from_list(input_list, item):
 
     ### YOUR CODE BELOW HERE ###
 
-    print("\nReplace this with your code!\n")
+    while item in input_list:
+        input_list.remove(item)
 
     ### YOUR CODE ABOVE HERE ###
 
@@ -42,7 +43,10 @@ def check_if_b_grade(grade):
 
     ### YOUR CODE BELOW HERE ###
 
-    print("\nReplace this with your code!\n")
+    if grade >= 80 and grade < 90:
+        is_b_grade = True
+    else:
+        is_b_grade = False
 
     ### YOUR CODE ABOVE HERE ###
 
@@ -63,7 +67,16 @@ def get_protein_seq(list_of_codons):
 
     ### YOUR CODE BELOW HERE ###
 
-    print("\nReplace this with your code!\n")
+    # creates an empty list to store the output
+    output_list = []
+    for codon in list_of_codons:
+         # check to see if each codon is in the codon dictionary, if it is add the corresponding one letter AA code to the ouput list
+        if codon in codon_dict:
+            output_list.append(codon_dict[codon])
+        # check to see if the codon is not in the codon dictionary, if it is not, return an empty list
+        else:
+            return []
+   
 
     ### YOUR CODE ABOVE HERE ###
     
@@ -75,11 +88,25 @@ def count_word_in_file(file_path, word_of_interest):
 
     ### YOUR CODE BELOW HERE ###
 
-    print("\nReplace this with your code!\n")
+    # starts a counter at 0
+    word_count = 0
+    # opens the file at the designated file path
+    with open(file_path, "r") as f:
+        for line in f:
+            # splits each line of the file into words
+            words = line.split()
+            for word in words:
+                # removes punctuation from the end of each word
+                word = word.strip(".,!?;:\"()")
+                # converts each word to lowercase
+                word = word.lower()
+                # checks to see if each word is the word of interest, if it is, add 1 to the counter
+                if word == word_of_interest:
+                    word_count += 1
+    # returns the final count of the word of interest in the file
+    return word_count
 
     ### YOUR CODE ABOVE HERE ###
-
-    return word_count
 
 
 # This function takes a list of 3 column names, and a list of data for each column (each data list is the same length), then outputs a correctly-formatted CSV file "data.csv".
@@ -87,7 +114,18 @@ def create_data_file(column_names_list, column1_data, column2_data, column3_data
 
     ### YOUR CODE BELOW HERE ###
 
-    print("\nReplace this with your code!\n")
+     # imports the csv module
+    import csv
+    # opens the file data.csv in write mode
+    with open("data.csv", "w", newline="") as data_file:
+        # creates a csv writer object
+        writer = csv.writer(data_file)
+        # writes the column names in column_names_list to the first row of the file
+        writer.writerow(column_names_list)
+        # writes the data in column1_data, column2_data, and column3_data to the file
+        # column1_data, column2_data, and column3_data are the same length, so the length of column1_data is used to determine how many rows to write to the file
+        for i in range(len(column1_data)):
+            writer.writerow([column1_data[i], column2_data[i], column3_data[i]])
 
     ### YOUR CODE ABOVE HERE ###
 
@@ -97,7 +135,32 @@ def filter_data():
 
     ### YOUR CODE BELOW HERE ###
 
-    print("\nReplace this with your code!\n")
+    # imports the csv module
+    import csv
+    # opens the file file2.csv in read mode
+    with open("file2.csv", "r") as input_file:
+        # creates a csv reader object for file2.csv
+        reader = csv.reader(input_file)
+        # reads the first row of the file and stores it in a variable called headers
+        headers = next(reader)
+        # opens the file tav.csv in write mode
+        with open("tav.csv", "w", newline="") as output_file1, open("andre.csv", "w", newline="") as output_file2:
+            # creates a csv writer object for tav.csv
+            writer1 = csv.writer(output_file1)
+            # creates a csv writer object for andre.csv
+            writer2 = csv.writer(output_file2)
+            # writes the headers to the first row of tav.csv
+            writer1.writerow(headers)
+            # writes the headers to the first row of andre.csv
+            writer2.writerow(headers)
+            # reads through the rest of the rows in the file
+            for row in reader:
+                # if the value in the fourth column of the row is "Tav", write the row to tav.csv
+                if row[3] == "Tav":
+                    writer1.writerow(row)
+                # if the value in the fourth column of the row is "Andre", write the row to andre.csv
+                elif row[3] == "Andre":
+                    writer2.writerow(row)
 
     ### YOUR CODE ABOVE HERE ###
 
